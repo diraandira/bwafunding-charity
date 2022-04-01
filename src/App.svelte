be7af03d32989baa42c0cf3027b2814142758d9e
Andira Romasari <andirarohmasari@gmail.com>
2022-04-01 19:19:12 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href= "https://svelte.dev/tetorial'>Svelte tutorial</a> to learn how to buid svelte apps.</p>
+</main>
+<footer>&copy: 2020 All right reserved.</footer>
+	
+<style>
+	main {
+		text-align: center;
+		paddin: lem;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	
+h1 {
+	color: #ff3ec00;
+	text-transfrom: uppercase;
+	font-size; 4em;
+	font-weight: 100;
+}
+div{
+	border: 0.5em solid #lalala;
+	padding: lem;
+}
+</style>
+
+<svelte:head>
+	<link
+	href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css"
+	rel="stylesheet" />
+</svelte:head>
+
+<div>
+	<h1>(title)</h1>
+	<CharityList charities={charities}/>
+</div>
---
+++ src/CharityList.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let charities;
+</script>
+<div>
+    <h2>Daftar Charity</h2>
+    <pre>Charities</pre>
+    <ul>
+    <li>Charity 1</li>
+    <li>Charity 2</li>
+</ul>
+</div>
+   
