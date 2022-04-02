6cb201e450bdfe428476a2a1125692d355e0d6c0
Andira Romasari <andirarohmasari@gmail.com>
2022-04-02 08:39:01 +0700

    perubahan pada App

--- src/App.svelte
+++ src/App.svelte
@@ -4,26 +4,26 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href= "https://svelte.dev/tetorial'>Svelte tutorial</a> to learn how to buid svelte apps.</p>
+	<p>Visit the <a href= "https://svelte.dev/tetorial">Svelte tutorial </a>/>
 </main>
 <footer>&copy: 2020 All right reserved.</footer>
 	
 <style>
 	main {
 		text-align: center;
-		paddin: lem;
+		padding: lem;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 	
 h1 {
-	color: #ff3ec00;
+	color: #ff3e00;
 	text-transfrom: uppercase;
-	font-size; 4em;
+	font-size: 4em;
 	font-weight: 100;
 }
 div{
-	border: 0.5em solid #lalala;
+	border: 0.5em solid #1a1a1a;
 	padding: lem;
 }
 </style>
